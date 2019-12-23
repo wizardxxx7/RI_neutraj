5e0fb3334f91937607fde3daca52a90cf57bcf94
wizardxxx7 <1901213554@pku.edu.cn>
2019-12-24 06:59:34 +0800

    python 3.7 and success

---
+++ train.py
@@ -0,0 +1,19 @@
+from geo_rnns.neutraj_trainer import NeuTrajTrainer
+from tools import config
+import os
+
+if __name__ == '__main__':
+    print('os.environ["CUDA_VISIBLE_DEVICES"]= {}'.format(os.environ["CUDA_VISIBLE_DEVICES"]))
+    print(config.config_to_str())
+
+
+
+    trajrnn = NeuTrajTrainer(tagset_size=config.d, batch_size=config.batch_size,
+                             sampling_num=config.sampling_num)
+    trajrnn.data_prepare(griddatapath=config.gridxypath, coordatapath=config.corrdatapath,
+                         distancepath=config.distancepath, train_radio=config.seeds_radio)
+    load_model_name = None
+
+    model_name = trajrnn.neutraj_train(load_model=None, in_cell_update=config.incell, save_model=True,
+                                       stard_LSTM=config.stard_unit)
+    trajrnn.trained_model_eval(load_model=model_name, in_cell_update=True, stard_LSTM=False)
